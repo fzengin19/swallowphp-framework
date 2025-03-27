73b40cfcee8fc22643d24d8ff850145b82c58771
Fatih Zengin <fatihzengin654@outlook.com>
2025-03-27 16:11:25 +0300

    Refactor(Exception): Remove redundant property from CsrfTokenMismatchException

--- src/Exceptions/CsrfTokenMismatchException.php
+++ src/Exceptions/CsrfTokenMismatchException.php
@@ -6,8 +6,6 @@ use Exception;
 
 class CsrfTokenMismatchException extends Exception
 {
-    public $code = 419; // Custom status code for CSRF errors
-  
     public function __construct($message = 'CSRF token mismatch', $code = 419, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
